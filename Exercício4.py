565feaadf05e7f68e6ccfd60ae46ff1065701cc6
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-13 09:41:05 -0300

    Início do exercício 4, onde precisa ser criado um programa para auxiliar um caixa com o troco;

---
+++ Exercício4.py
@@ -0,0 +1,5 @@
+# Um caixa precisa de um programa que o auxilie no fornecimento de valores com o mínimo
+# de cédulas/moedas possíveis. Monte um programa que seja fornecido o valor (inteiro) a
+# ser fornecido e tenha como saída o total de cédulas utilizadas, nos valores de 100, 50, 20,
+# 10, 5, 2 e 1.
+
