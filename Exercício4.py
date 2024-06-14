ae4e268bd1b042dea0cdd148cf3bc212395ac7ae
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-14 09:26:32 -0300

    Criada a função que valida a entreda do usuário se é um número inteiro e se tambem a execusão do fim do programa.

--- Exercício4.py
+++ Exercício4.py
@@ -5,8 +5,32 @@
 
 from FuncoesBasicas import printAbertura as Abertura
 
+# Função que valida a entrada do usuário e converte em inteiro.
+def EnumInterio(entrada):
+    while True:
+        valor = input(entrada)
+        
+        try:
+            valor = int(valor)
+            
+            if (valor == 999):
+                print("\033[1;30;41mEntrada 999 detectada, saindo...\033[m")
+                exit()
+            
+            if (valor <= 0):
+                print("\033[1;31mEntrada inválida\033[m, favor digite um número inteiro \033[1;32mpositivo!\033[m ")
+            else:
+                return valor
+        
+        except ValueError:
+            print("\033[1;31mEntrada inválida\033[m, favor digite um número inteiro: ")
+
+
+# Inicio do programa
 Abertura("contador de notas")
+print('Para sair digite 999')
 
 cedulas = [100, 50, 20, 10, 5, 2, 1]
 
-num = input("Digite um valor inteiro para calcular a quantidade de notas: ")
+num = EnumInterio("Digite um valor inteiro para calcular a quantidade de notas: ")
+
