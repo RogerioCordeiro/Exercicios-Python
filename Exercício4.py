edc420e9f80e40cf2a8c37241a30343e78265b54
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-14 09:11:55 -0300

    Inicio do exercicio 4, separador de cedulas.

--- Exercício4.py
+++ Exercício4.py
@@ -3,3 +3,10 @@
 # ser fornecido e tenha como saída o total de cédulas utilizadas, nos valores de 100, 50, 20,
 # 10, 5, 2 e 1.
 
+from FuncoesBasicas import printAbertura as Abertura
+
+Abertura("contador de notas")
+
+cedulas = [100, 50, 20, 10, 5, 2, 1]
+
+num = input("Digite um valor inteiro para calcular a quantidade de notas: ")
