28431c8684bd641a8eaf9c3b5020fe71f6953aeb
Rogerio <Rogerio@MACK2>
2024-06-14 09:57:24 -0300

    Finalizado o exercício 4, ultima parte do codigo, separando a quantidade de notas e correções na validação das entradas, com alteração do comando para sair de 999 para sair.

--- Exercício4.py
+++ Exercício4.py
@@ -4,33 +4,47 @@
 # 10, 5, 2 e 1.
 
 from FuncoesBasicas import printAbertura as Abertura
+import time
 
 # Função que valida a entrada do usuário e converte em inteiro.
 def EnumInterio(entrada):
     while True:
+        print('\033[1;33mPara sair digite "SAIR"\033[m')
         valor = input(entrada)
         
         try:
+            if (valor.lower() == 'sair'):
+                print("\033[1;30;41mSeu pedido é uma ordem, saindo...\033[m")
+                Abertura("obrigado, até mais!")
+                exit()
+                
             valor = int(valor)
             
-            if (valor == 999):
-                print("\033[1;30;41mEntrada 999 detectada, saindo...\033[m")
-                exit()
-            
             if (valor <= 0):
-                print("\033[1;31mEntrada inválida\033[m, favor digite um número inteiro \033[1;32mpositivo!\033[m ")
+                print("\033[1;31mEntrada inválida\033[m, favor digite um número inteiro \033[1;32mpositivo!\033[m \n")
+                time.sleep(2)
             else:
                 return valor
         
         except ValueError:
-            print("\033[1;31mEntrada inválida\033[m, favor digite um número inteiro: ")
-
+            print("\033[1;31mEntrada inválida\033[m, favor digite um número inteiro: \n")
+            time.sleep(2)
 
 # Inicio do programa
 Abertura("contador de notas")
-print('Para sair digite 999')
 
 cedulas = [100, 50, 20, 10, 5, 2, 1]
 
-num = EnumInterio("Digite um valor inteiro para calcular a quantidade de notas: ")
-
+while True:
+    num = EnumInterio("Digite um valor inteiro para calcular a quantidade de notas: ")
+   
+    for cedula in cedulas:
+        quantidade = int(num / cedula)
+        
+        if (quantidade > 0 ):
+            print(f'{quantidade} cedúla(s) de R$ {cedula}!')
+        
+        num %= cedula
+    
+    print()
+    
